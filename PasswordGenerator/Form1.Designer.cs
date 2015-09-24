c62db2a2dd893a6c72953d2b358058a9814021e6
Zhai YiMing <tinymins@gmail.com>
2015-09-24 20:24:31 +0800

    First commit.

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textAccount = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.textSalt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textAccount
+            // 
+            this.textAccount.Location = new System.Drawing.Point(12, 12);
+            this.textAccount.Name = "textAccount";
+            this.textAccount.Size = new System.Drawing.Size(270, 20);
+            this.textAccount.TabIndex = 1;
+            this.textAccount.TextChanged += new System.EventHandler(this.textAccount_TextChanged);
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(12, 65);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.ReadOnly = true;
+            this.textPassword.Size = new System.Drawing.Size(270, 20);
+            this.textPassword.TabIndex = 2;
+            // 
+            // textSalt
+            // 
+            this.textSalt.Location = new System.Drawing.Point(11, 39);
+            this.textSalt.Name = "textSalt";
+            this.textSalt.PasswordChar = '*';
+            this.textSalt.Size = new System.Drawing.Size(271, 20);
+            this.textSalt.TabIndex = 0;
+            this.textSalt.TextChanged += new System.EventHandler(this.textSalt_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 100);
+            this.Controls.Add(this.textSalt);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textAccount);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textAccount;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.TextBox textSalt;
+    }
+}
+
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;  
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generatePassword()
+        {
+            string salt = textSalt.Text.Trim();
+            string account = textAccount.Text.Trim();
+            if (salt.Equals("") || account.Equals(""))
+                return;
+
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] buffer = Encoding.Default.GetBytes(account + salt);
+            buffer = md5.ComputeHash(buffer);
+            buffer = md5.ComputeHash(buffer);
+            buffer = md5.ComputeHash(buffer);
+            string password = BitConverter.ToString(buffer).Replace("-", "").ToLower();
+            password = password.Substring(1, 10);
+            textPassword.Text = password;
+        }
+
+        private void textAccount_TextChanged(object sender, EventArgs e)
+        {
+            generatePassword();
+        }
+        
+        private void textSalt_TextChanged(object sender, EventArgs e)
+        {
+            generatePassword();
+        }
+    }
+}
